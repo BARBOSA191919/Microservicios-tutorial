f43d00a55ccfb222fa30967bc8675171c9d2b71d
BARBOSA191919 <sbarbosarivas@gmail.com>
2024-02-06 11:51:27 -0500

    eureka

--- eureka-service/src/test/java/com/eureka/service/EurekaServiceApplicationTests.java
+++ eureka-service/src/test/java/com/eureka/service/EurekaServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.config.service;
+package com.eureka.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServiceApplicationTests {
+class EurekaServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
